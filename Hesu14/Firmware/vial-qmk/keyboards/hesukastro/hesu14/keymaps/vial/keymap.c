bb5ce0574b501d1c10e8a7e566ccffd675383ce6
Jesus Castro <jjoesei.castro@gmail.com>
2025-08-18 04:26:12 +0800

    Cleanup

--- Hesu14/Firmware/vial-qmk/keyboards/hesukastro/hesu14/keymaps/vial/keymap.c
+++ Hesu14/Firmware/vial-qmk/keyboards/hesukastro/hesu14/keymaps/vial/keymap.c
@@ -3,29 +3,45 @@
 #include <qp.h>
 #include "animations.h"
 
+/* ------------------------- User Config ------------------------- */
+typedef union {
+    uint32_t raw;
+    struct {
+        uint8_t current_image_index;
+        bool is_using_AHK;
+    };
+} user_config_t;
+
+user_config_t user_config;
+
+/* ------------------------- Image Data ------------------------- */
 const uint8_t * const gfx_images[] = {
     gfx_togepi_128x32,
     gfx_yibbygibby,
     gfx_bonggocat
 };
 
-enum qp_keycode {
-    QP_NEXT = QK_KB_0,
-    QP_PREV
-};
-
 #define NUM_IMAGES (sizeof(gfx_images) / sizeof(gfx_images[0]))
 
-painter_device_t display;
+/* ------------------------- Custom Keycodes ------------------------- */
+enum custom_keycodes {
+    QP_NEXT = QK_KB_0,
+    QP_PREV,
+    TOG_AHK
+};
 
+/* ------------------------- Display / Animation ------------------------- */
+painter_device_t display;
 static painter_image_handle_t my_image = NULL;
 static deferred_token my_anim = INVALID_DEFERRED_TOKEN;
-static uint8_t current_image_index = 0;
 static uint16_t last_anim_switch = 0;
 
+/* ------------------------- Functions ------------------------- */
 void eeconfig_init_user(void) {
-    current_image_index = 0;
-    eeconfig_update_user(current_image_index);
+    user_config.raw = 0;
+    user_config.current_image_index = 0;
+    user_config.is_using_AHK = false;
+    eeconfig_update_user(user_config.raw);
 }
 
 bool can_switch_anim(void) {
@@ -52,70 +68,77 @@ void load_animation(uint8_t index) {
 
     if (my_image) {
         my_anim = qp_animate(display, 0, 0, my_image);
-        eeconfig_update_user(current_image_index);
+        user_config.current_image_index = index;
+        eeconfig_update_user(user_config.raw);
     }
 }
 
+/* ------------------------- Init ------------------------- */
 void keyboard_post_init_kb(void) {
     display = qp_sh1106_make_i2c_device(128, 32, 0x3c);
     qp_init(display, QP_ROTATION_180);
     qp_power(display, true);
 
-    current_image_index = eeconfig_read_user();
-    if (current_image_index >= NUM_IMAGES) {
-        current_image_index = 0;
+    user_config.raw = eeconfig_read_user();
+    if (user_config.current_image_index >= NUM_IMAGES) {
+        user_config.current_image_index = 0;
     }
 
-    load_animation(current_image_index);
+    load_animation(user_config.current_image_index);
 }
 
+/* ------------------------- Key Processing ------------------------- */
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch(keycode) {
+    switch (keycode) {
         case MO(1):
-            if(record->event.pressed) {
+            if (!user_config.is_using_AHK) break;
+            if (record->event.pressed) {
                 register_code(KC_F23);
             } else {
                 unregister_code(KC_F23);
             }
             break;
+
         case MO(2):
-            if(record->event.pressed) {
+            if (!user_config.is_using_AHK) break;
+            if (record->event.pressed) {
                 register_code(KC_F24);
             } else {
                 unregister_code(KC_F24);
             }
+            break;
+
         case QP_NEXT:
-            if(record->event.pressed && can_switch_anim()){
-                current_image_index = (current_image_index + 1) % NUM_IMAGES;
-                load_animation(current_image_index);
+            if (record->event.pressed && can_switch_anim()) {
+                user_config.current_image_index =
+                    (user_config.current_image_index + 1) % NUM_IMAGES;
+                load_animation(user_config.current_image_index);
             }
             break;
+
         case QP_PREV:
-            if(record->event.pressed && can_switch_anim()){
-                if (current_image_index == 0) {
-                    current_image_index = NUM_IMAGES - 1;
+            if (record->event.pressed && can_switch_anim()) {
+                if (user_config.current_image_index == 0) {
+                    user_config.current_image_index = NUM_IMAGES - 1;
                 } else {
-                    current_image_index--;
+                    user_config.current_image_index--;
                 }
-                load_animation(current_image_index);
+                load_animation(user_config.current_image_index);
+            }
+            break;
+
+        case TOG_AHK:
+            if (record->event.pressed) {
+                user_config.is_using_AHK = !user_config.is_using_AHK;
+                eeconfig_update_user(user_config.raw);
             }
             break;
     }
     return true;
 }
 
+/* ------------------------- Keymaps ------------------------- */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┐
-     * │ OLED  │ 1 │ 2 │
-     * ├───┼───┼───┼───┤
-     * │ 3 │ 4 │ 5 │ 6 │
-     * ├───┼───┼───┼───┤
-     * │ 7 │ 8 │ 9 │ 0 │
-     * ├───┼───┼───┼───┤
-     * │ B │ B │ C │ D │
-     * └───┴───┴───┴───┘
-     */
     [0] = LAYOUT(
                         QP_PREV,   QP_NEXT,
         KC_1,   KC_2,   KC_3,   KC_4,
